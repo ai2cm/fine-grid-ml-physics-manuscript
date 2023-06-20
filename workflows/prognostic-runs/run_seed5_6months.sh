b745cf7e91d652d73c277a8b69c0eb87e5197d36
Oliver Watt-Meyer <oliverwatt@gmail.com>
2023-06-20 15:50:58 -0700

    Add script for running seed 5 prog run for 6 months

---
+++ workflows/prognostic-runs/run_seed5_6months.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+BUCKET=vcm-ml-experiments # don't pass bucket arg to use default 'vcm-ml-experiments'
+PROJECT=default # don't pass project arg to use default 'default'
+RANDOM_SEED=5
+
+MODEL_URL="gs://vcm-ml-experiments/default/2022-09-20/ml-on-fine-res-train-nn-precip-rs-trial-0/trained_models/model${RANDOM_SEED}"
+EXPERIMENT="ml-on-fine-ensemble-prog-rad-vl-${RANDOM_SEED}"
+TRIAL="trial-0"
+TAG=${EXPERIMENT}-${TRIAL}  # required
+NAME="${TAG}-$(openssl rand --hex 2)"
+
+cp prognostic-run.yaml prognostic-run-compiled.yaml
+yq -yi --arg url $MODEL_URL '.online_emulator.url[0]|=$url' prognostic-run-compiled.yaml
+
+argo submit --from workflowtemplate/prognostic-run \
+    -p bucket=${BUCKET} \
+    -p project=${PROJECT} \
+    -p tag=${TAG} \
+    -p config="$(< prognostic-run-compiled.yaml)" \
+    -p memory="25Gi" \
+    -p cpu="24" \
+    -p segment-count=36 \
+    --name "${NAME}" \
+    --labels "project=${PROJECT},experiment=${EXPERIMENT},trial=${TRIAL}"
